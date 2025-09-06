b6915aed88e68d3a8791d7d5b182882a87a8c42f
vishnuoum <vishnuoumvishnuoum123@gmail.com>
2025-09-06 16:17:52 +0530

    Silver Quality Check header updates

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,3 +1,22 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
 -- ========CRM Customer Info Table QCs===========
 
 -- Check for Nulls or duplicates in Primary Key
